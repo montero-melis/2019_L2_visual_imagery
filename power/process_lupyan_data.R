2f6b284fd77e12c13bbde6622b930a7d99bf9c8c
montero-melis <montero.gui@gmail.com>
2019-11-08 18:02:16 +0100

    Adds some variables and renames others

--- power/process_lupyan_data.R
+++ power/process_lupyan_data.R
@@ -17,7 +17,7 @@ names(exp1)
 # rename some columns
 exp1 <- exp1 %>%
   rename(
-    participant = subjCode, trial = trial_num, prime = sound_name, picture = pic_name
+    participant = subjCode, trial = trial_num, prime = sound_name, target = pic_name
   )
 head(exp1)
 
@@ -29,24 +29,25 @@ exp1 %>% select(is_valid, is_pres, cue) %>% unique() %>% arrange(is_valid, is_pr
 # bit misleading since any prompt is considered as "valid" for the no-object
 # trials, the "validity_recoded" column might have addressed this...
 exp1 %>%
-  group_by(validity_prompt, picture, cue, prime, is_pres, is_valid, validity_recoded) %>%
+  group_by(validity_prompt, target, cue, prime, is_pres, is_valid, validity_recoded) %>%
   count() %>%
-  arrange(validity_prompt, picture, is_pres, is_valid, validity_recoded) %>%
+  arrange(validity_prompt, target, is_pres, is_valid, validity_recoded) %>%
   print(n = 22)
 # oh man, this seems quite convoluted and their might be some errors in there...
 #
-exp1 %>% select(prime, picture) %>% unique() %>% arrange(prime, picture) %>% print(n=50)
+exp1 %>% select(prime, target) %>% unique() %>% arrange(prime, target) %>% print(n=50)
 
 
 # Create columns for compatibility with Markus's data
 exp1 <- exp1 %>%
-  mutate(condition = ifelse(prime == picture, "congruent", "incongruent"))
+  mutate(condition = ifelse(prime == target, "congruent", "incongruent"))
 exp1$condition[is.na(exp1$condition)] <- "incongruent"
 exp1$condition[exp1$prime == "noise"] <- "noise"
 # check result
-exp1 %>% select(prime, picture, condition) %>% unique() %>%
-  arrange(prime, picture, condition) %>% print(n=20)
-# pic_presence
+exp1 %>% select(prime, target, condition) %>% unique() %>%
+  arrange(prime, target, condition) %>% print(n=20)
+
+# pic_presence and condition
 table(exp1$condition)
 table(exp1$is_pres)
 with(exp1, addmargins(table(condition, is_pres)))
@@ -56,7 +57,7 @@ with(exp1, addmargins(table(condition, is_pres)))
 exp1_simple <- exp1 %>%
   select(
     participant, block, trial, condition, pic_presence = is_pres, cue, prime,
-    target = picture, resp_acc = obj_presence_acc, RT = rt
+    target, resp_acc = obj_presence_acc, RT = rt
   )
 head(exp1_simple)
 
@@ -69,27 +70,39 @@ write_csv(exp1_simple, "power/data_lupyan13_exp1_simple.csv")
 names(exp2)
 
 unique(exp2$sound)
-table(exp2$sound)
-# what's this "is_valid" column?
-exp2 %>% select(is_valid, is_pres) %>% unique() %>% arrange(is_valid, is_pres)
-exp2 %>% select(is_valid, is_pres, cue) %>% unique() %>% arrange(is_valid, is_pres, cue)
+sort(table(exp2$sound))
 # There were 4 exemplars of each object category (see LW13, p.14200)
 exp2 %>% select(pic_name, pic_num) %>% unique() %>% arrange(pic_name, pic_num)
 
 # rename some columns
 exp2 <- exp2 %>%
   rename(
-    participant = subjCode, trial = trial_num, picture = pic_name,
+    participant = subjCode, trial = trial_num, prime = sound, target = pic_name
   ) %>%
-  mutate(picture_version = paste(picture, pic_num, sep = "_"))
+  mutate(target_version = paste(target, pic_num, sep = "_"))
 head(exp2)
+
+exp2 <- exp2 %>%
+  mutate(condition = ifelse(prime == target, "congruent", "incongruent"))
+exp2$condition[is.na(exp2$condition)] <- "incongruent"
+exp2$condition[exp2$prime == "noise"] <- "noise"
+# check result
+exp2 %>% select(prime, target, condition) %>% unique() %>%
+  arrange(prime, target, condition) %>% print(n=20)
+
+# pic_presence and condition
+table(exp2$condition)
+table(exp2$is_pres)
+with(exp2, addmargins(table(condition, is_pres)))
+
 
 # Simplified version of data, enough to compute d' and make it compatible with
 # Markus's data:
 exp2_simple <- exp2 %>%
+  mutate(RT = 1000 * rt) %>%
   select(
-    participant, block, trial, prime = sound, picture, picture_version, cue,
-    is_pres, obj_presence_acc
+    participant, block, trial, condition, pic_presence = is_pres, cue, prime,
+    target, target_version, resp_acc = obj_presence_acc, RT
   )
 head(exp2_simple)
 
